0bae903ea04cf14f0d040b58563ce3f5c5430863
Navong <navongbcvbong@gmail.com>
2024-05-07 23:54:11 +0900

    change convex source

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://mutual-hamster-59.clerk.accounts.dev",
+      domain: "https://hip-sloth-74.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
