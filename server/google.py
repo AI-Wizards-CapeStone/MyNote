deedfdd04f6ef7de64f39501313f7d7d495ecefe
Navong <navongbcvbong@gmail.com>
2024-06-14 04:50:01 +0900

    latex-api

---
+++ server/google.py
@@ -0,0 +1,53 @@
+import os
+import time
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyC5xHls3H-KrSXSDBjeExZvrzeZzrAwlkA")
+
+def upload_to_gemini(path, mime_type=None):
+  """Uploads the given file to Gemini.
+
+  See https://ai.google.dev/gemini-api/docs/prompting_with_media
+  """
+  file = genai.upload_file(path, mime_type=mime_type)
+  print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+  return file
+
+def wait_for_files_active(files):
+  print("Waiting for file processing...")
+  for name in (file.name for file in files):
+    file = genai.get_file(name)
+    while file.state.name == "PROCESSING":
+      print(".", end="", flush=True)
+      time.sleep(10)
+      file = genai.get_file(name)
+    if file.state.name != "ACTIVE":
+      raise Exception(f"File {file.name} failed to process")
+  print("...all files ready")
+  print()
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+# Replace "path/to/your/image.jpg" with the actual path to your image
+sample_file = upload_to_gemini("server/image.png", mime_type="image/png")
+
+# Wait for the image to be processed 
+wait_for_files_active([sample_file])
+
+response = model.generate_content([sample_file, "Generate Latex code from this image"])
+
+print(response.text)
